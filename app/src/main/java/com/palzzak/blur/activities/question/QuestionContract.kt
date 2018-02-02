cc128ca75a52541a6d30e20d8473b1668687747c
heeinso <heeinso@snu.ac.kr>
2018-02-02 23:53:14 +0900

    Add question activities

---
+++ app/src/main/java/com/palzzak/blur/activities/question/QuestionContract.kt
@@ -0,0 +1,18 @@
+package com.palzzak.blur.activities.question
+
+import com.palzzak.blur.BasePresenter
+import com.palzzak.blur.BaseView
+
+/**
+ * Created by stevehan on 2018. 2. 1..
+ */
+
+interface QuestionContract {
+    interface Presenter: BasePresenter<View> {
+
+    }
+
+    interface View: BaseView<Presenter> {
+
+    }
+}
---
+++ app/src/main/java/com/palzzak/blur/activities/question/QuestionModule.kt
@@ -0,0 +1,5 @@
+package com.palzzak.blur.activities.question
+
+/**
+ * Created by stevehan on 2018. 2. 1..
+ */
