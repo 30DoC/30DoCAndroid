a1165e24c23b76915b36667ce2971f97e38c3e9a
heeinso <heeinso@snu.ac.kr>
2018-02-09 19:05:40 +0900

    Add APIService to QuestionPresenter roughly

--- app/src/main/java/com/palzzak/blur/ui/question/QuestionPresenter.kt
+++ app/src/main/java/com/palzzak/blur/ui/question/QuestionPresenter.kt
@@ -1,6 +1,7 @@
 package com.palzzak.blur.ui.question
 
 import com.palzzak.blur.di.PerActivity
+import com.palzzak.blur.network.APIService
 import com.palzzak.blur.network.response.Question
 import java.util.*
 import javax.inject.Inject
@@ -13,6 +14,9 @@ class QuestionPresenter @Inject constructor(): QuestionContract.Presenter {
     @Inject
     lateinit var mQuestionView: QuestionContract.View
 
+    @Inject
+    lateinit var mAPIService: APIService
+
 
     override fun printQuestionDescription() {
         var numberOfQuestions = 10
@@ -24,7 +28,7 @@ class QuestionPresenter @Inject constructor(): QuestionContract.Presenter {
         val questions = arrayListOf<Question>()
 
         for (i in 1..10) {
-            questions.add(Question(true, "", i, Date(), Date()))
+            questions.add(Question("질문 예시입니다", true, i, Date(), Date()))
         }
 
         mQuestionView.setQuestions(questions)
