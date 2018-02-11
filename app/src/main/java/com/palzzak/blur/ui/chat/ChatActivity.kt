d8720be2513a77e8bda6c7fc847c5c3a43420ee4
yooasd1 <yooasd1@gmail.com>
2018-02-11 22:06:46 +0900

    Add chat layout

--- app/src/main/java/com/palzzak/blur/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/palzzak/blur/ui/chat/ChatActivity.kt
@@ -1,15 +1,21 @@
 package com.palzzak.blur.ui.chat
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import com.palzzak.blur.R
+import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
 /**
  * Created by jaeyoonyoo on 2018. 2. 11..
  */
-class ChatActivity: AppCompatActivity(), ChatContract.View{
+class ChatActivity: DaggerAppCompatActivity(), ChatContract.View{
     @Inject
     lateinit var mChatPresenter: ChatPresenter
 
-
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
 }
